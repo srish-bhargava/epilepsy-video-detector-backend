240abe65e3ed076ba9198e5f8414154dc71db79f
malfusion <judenaveenraj@gmail.com>
2021-03-13 21:52:08 -0500

    Added gitignore file and added video filename hashing

--- processor.py
+++ processor.py
@@ -1,12 +1,24 @@
 from pytube import YouTube
 import constants
+import hashlib
+import os
 
 def processVideo(url):
     downloadVideo(url)
     return None
 
 def downloadVideo(url): 
+    filename = getHash(url)
+    filepath = constants.VIDEO_DIRECTORY + "/" + filename
+    
+    if os.path.exists(filepath + ".mp4"):
+        return filepath
+
     yt = YouTube(url)
     stream = yt.streams.first()
-    stream.download(constants.VIDEO_DIRECTORY)
+    stream.download(constants.VIDEO_DIRECTORY, filename)
+    return filepath
 
+def getHash(s):
+    hash_object = hashlib.md5(s.encode())
+    return hash_object.hexdigest()
