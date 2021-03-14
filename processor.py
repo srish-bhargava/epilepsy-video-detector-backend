9ebe2a66b039520116936d52cc258c70df2d4b56
Srishti Bhargava <srishtibhargava@Srishtis-MacBook-Pro.local>
2021-03-13 22:01:49 -0500

    Added opencv luminance processor

--- processor.py
+++ processor.py
@@ -2,9 +2,13 @@ from pytube import YouTube
 import constants
 import hashlib
 import os
+import cv2
+from PIL import Image, ImageStat
 
 def processVideo(url):
-    downloadVideo(url)
+    filepath = downloadVideo(url)
+    lums = analyseVideoLuminance(filepath)
+    print (lums)
     return None
 
 def downloadVideo(url): 
@@ -17,8 +21,28 @@ def downloadVideo(url):
     yt = YouTube(url)
     stream = yt.streams.first()
     stream.download(constants.VIDEO_DIRECTORY, filename)
-    return filepath
+    return filepath+".mp4"
 
 def getHash(s):
     hash_object = hashlib.md5(s.encode())
     return hash_object.hexdigest()
+
+def analyseVideoLuminance(filepath):
+    lums = []
+    cap = cv2.VideoCapture(filepath)
+   
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        im_pil = Image.fromarray(frame).convert('L')
+        lum = ImageStat.Stat(im_pil).mean[0]
+        lums.append(lum)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return lums
+
