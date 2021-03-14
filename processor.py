42a419c0a3915ef21c83bce38a4a9ff3082d2b80
Srishti Bhargava <srishtibhargava@Srishtis-MacBook-Pro.local>
2021-03-13 21:34:49 -0500

    Download video from youtube

--- processor.py
+++ processor.py
@@ -1,4 +1,12 @@
-
+from pytube import YouTube
 
 def processVideo(url):
+    downloadVideo(url)
     return None
+
+def downloadVideo(url):
+    SAVE_PATH = "./" 
+    yt = YouTube(url)
+    stream = yt.streams.first()
+    stream.download(SAVE_PATH)
+
